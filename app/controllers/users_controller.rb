91348fdfec358108056d6661adb5aa37730d77c4
佐々木　暁 <akirasasaki@MacBook-Pro-731.local>
2018-07-09 14:54:27 +0800

    Making profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-  	@users = User.all
+    @users = User.all
   end
 
   def show
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.create(
+    @user = User.create(
       name: params[:name],
       email: params[:email],
       password: params[:password]
       )
-    	if @user.save
+      if @user.save
         session[:user_id] = @user.id
         flash[:notice] = "Welcome to English e-learning!"
         redirect_to("/users/#{@user.id}")
