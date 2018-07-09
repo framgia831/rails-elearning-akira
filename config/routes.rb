93bcdcd7c9a29159b1329240b53ef7b52f171a74
akira1105 <40820396+akira1105@users.noreply.github.com>
2018-07-09 16:17:41 +0800

    Making profile page (#5)
    * Making profile page
    
    * Making profile page
    
    * Making profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   get 	'/users/index'
   get 	'/signup', to: 'users#new'
-  post 	'/users/create', to: 'users#create'
-  get 	'/users/:id', to: 'users#show'
+  
 
   root  'static_pages#home'
   get   '/about', to: 'static_pages#about'
+
+  resources :users, except: :new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
