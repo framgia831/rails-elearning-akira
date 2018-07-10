54721cd42d4c48a32c24f21c260e6ac00e777eb0
佐々木　暁 <akirasasaki@MacBook-Pro-731.local>
2018-07-10 15:30:41 +0800

    login logout function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
     
-  before_action :set_current_user
+  include SessionHelper
 
-  def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
+  def require_login
+    unless logged_in?
+        store_location
+        flash[:notice] = "Please log in."
+        redirect_to login_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
 
+  get 'session/new'
   get 	'/users/index'
   get 	'/signup', to: 'users#new'
-  
 
+  get 		'/login', to: 'session#new'
+  post 		'/login', to: 'session#create'
+  delete	'/logout', to: 'session#destroy'
+  
   root  'static_pages#home'
   get   '/about', to: 'static_pages#about'
 
